d74bf266f0b030fd4b1b536e6dad865689079c99
Perez <DPerez1@int.asurion.com>
2020-06-12 15:07:52 +0900

    added two tests

--- test.js
+++ test.js
@@ -22,18 +22,32 @@ describe('Electron app tests', function () {
     });
 
     it('display the electron app window', async function() {
-        const count = await app.client.getWindowCount();
-        return assert.equal(count, 1)
+        let windowCount = await app.client
+        .waitUntilWindowLoaded()
+        .getWindowCount();
+        return assert.equal(windowCount, 1)
     });
 
     it('displays a title', async function() {
-        app.client.waitUntilWindowLoaded();
-        const title = await app.client.getTitle();
-        return assert.equal(title, 'Metis')
+        let titleName = await app.client
+        .waitUntilWindowLoaded()
+        .getTitle();
+
+        return assert.equal(titleName, 'Metis')
     });
 
-    it('clickFile', async function() {
-        app.client.waitUntilWindowLoaded();
+    it('columns display', async function() {
+        let isColumnsVisible = await app.client
+        .waitUntilWindowLoaded()
+        .isVisible('.column');
+        return assert.ok(isColumnsVisible); //assert.ok checks if value is true. 
     });
+
+    it('confirm text on column', async function() {
+      let columnText = await app.client
+      .waitUntilWindowLoaded()
+      .getText('//*[@id="root"]/div/div/div[2]/div[2]/div/div/div/div[1]/div/div[2]/div/div/div/p'); // p tag with in the column. 
+      return assert.equal(columnText, "Please connect your device"); 
+  });
 
 });
